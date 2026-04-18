2b7956cc5d9c0fe2009e9eef2b88a20dc7d9e9b0
Okhrymenko <Ohrimenko15@gmail.com>
2026-04-18 17:26:30 +0300

    create the structure of the project

---
+++ config.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://www.work.ua/jobs-python/"
+
+
+TECHNOLOGIES = [
+    "Python", "Django", "Flask", "FastAPI", "PostgreSQL",
+    "Docker", "AWS", "Git", "Redis", "SQL", "Linux",
+    "Celery", "Asynchronous", "REST", "NoSQL"
+]
